ff574b93770c8becf80da27365fd03a3ad9c897e
GgnDpSngh <shanky.22.singh@gmail.com>
2020-08-26 17:57:48 +0200

    rearranged files

--- tf_verify/scripts/run_acasxu.sh
+++ tf_verify/scripts/run_acasxu.sh
@@ -1,7 +1,7 @@
 !/bin/bash
 #L_infinity attack
 rm results/acasxu/prop*
-for net in ../data/acasxu/nets/*.onnx;
+for net in ../../data/acasxu/nets/*.onnx;
 do
 
 timeout 300 python3 -u . --netname $net --specnumber 1 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10 >> results/acasxu/prop1.txt
@@ -9,7 +9,7 @@ timeout 300 python3 -u . --netname $net --specnumber 1 --domain deeppoly --datas
 	
 done
 
-for net in ../data/acasxu/nets/*.onnx;
+for net in ../../data/acasxu/nets/*.onnx;
 do
 
         timeout 300 python3 -u . --netname $net --specnumber 2 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10 >> results/acasxu/prop2.txt
@@ -17,7 +17,7 @@ do
 	
 done
 
-for net in ../data/acasxu/nets/*.onnx;
+for net in ../../data/acasxu/nets/*.onnx;
 do
 
         timeout 300 python3 -u . --netname $net --specnumber 3 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10 >> results/acasxu/prop3.txt
@@ -25,7 +25,7 @@ do
 	
 done
 
-for net in ../data/acasxu/nets/*.onnx;
+for net in ../../data/acasxu/nets/*.onnx;
 do
 
         timeout 300 python3 -u . --netname $net --specnumber 4 --domain deeppoly --dataset acasxu --complete True  --timeout_milp 10 >> results/acasxu/prop4.txt
@@ -34,14 +34,14 @@ do
 done
 
 
-timeout 300 python3 -u . --netname ../data/acasxu/nets/ACASXU_run2a_1_1_batch_2000.onnx --specnumber 5 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop5.txt
+timeout 300 python3 -u . --netname ../../data/acasxu/nets/ACASXU_run2a_1_1_batch_2000.onnx --specnumber 5 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop5.txt
 
-timeout 300 python3 -u . --netname ../data/acasxu/nets/ACASXU_run2a_1_1_batch_2000.onnx --specnumber 6 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10 > results/acasxu/prop6.txt
+timeout 300 python3 -u . --netname ../../data/acasxu/nets/ACASXU_run2a_1_1_batch_2000.onnx --specnumber 6 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10 > results/acasxu/prop6.txt
 
-timeout 300 python3 -u . --netname ../data/acasxu/nets/ACASXU_run2a_1_9_batch_2000.onnx --specnumber 7 --domain deeppoly --dataset acasxu --complete True --timeout_milp 1  > results/acasxu/prop7.txt
+timeout 300 python3 -u . --netname ../../data/acasxu/nets/ACASXU_run2a_1_9_batch_2000.onnx --specnumber 7 --domain deeppoly --dataset acasxu --complete True --timeout_milp 1  > results/acasxu/prop7.txt
 
-timeout 300 python3 -u . --netname ../data/acasxu/nets/ACASXU_run2a_2_9_batch_2000.onnx --specnumber 8 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop8.txt
+timeout 300 python3 -u . --netname ../../data/acasxu/nets/ACASXU_run2a_2_9_batch_2000.onnx --specnumber 8 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop8.txt
 
-timeout 300 python3 -u . --netname ../data/acasxu/nets/ACASXU_run2a_3_3_batch_2000.onnx --specnumber 9 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop9.txt
+timeout 300 python3 -u . --netname ../../data/acasxu/nets/ACASXU_run2a_3_3_batch_2000.onnx --specnumber 9 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop9.txt
 
-timeout 300 python3 -u . --netname ../data/acasxu/nets/ACASXU_run2a_4_5_batch_2000.onnx --specnumber 10 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop10.txt
+timeout 300 python3 -u . --netname ../../data/acasxu/nets/ACASXU_run2a_4_5_batch_2000.onnx --specnumber 10 --domain deeppoly --dataset acasxu --complete True --timeout_milp 10  > results/acasxu/prop10.txt
--- tf_verify/scripts/run_ffn_relu.sh
+++ tf_verify/scripts/run_ffn_relu.sh
@@ -1,12 +1,12 @@
-python3 -u . --netname  mnist-net_256x2.onnx --dataset mnist --domain deeppoly --epsilon 0.02 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x2_eps0.02.txt
+python3 -u . --netname  ../relu_image_models/ffn_relu/mnist-net_256x2.onnx --dataset mnist --domain deeppoly --epsilon 0.02 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x2_eps0.02.txt
 
-python3 -u . --netname  mnist-net_256x2.onnx --dataset mnist --domain deeppoly --epsilon 0.05 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x2_eps0.05.txt
+python3 -u . --netname  ../relu_image_models/ffn_relu/mnist-net_256x2.onnx --dataset mnist --domain deeppoly --epsilon 0.05 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x2_eps0.05.txt
 
-python3 -u . --netname  mnist-net_256x4.onnx --dataset mnist --domain deeppoly --epsilon 0.02 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x4_eps0.02.txt
+python3 -u . --netname  ../relu_image_models/ffn_relu/mnist-net_256x4.onnx --dataset mnist --domain deeppoly --epsilon 0.02 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x4_eps0.02.txt
 
-python3 -u . --netname  mnist-net_256x4.onnx --dataset mnist --domain deeppoly --epsilon 0.05 --complete True --timeout_milp 90 --subset comp  > results/FFN_relu/mnist_256x4_eps0.05.txt
+python3 -u . --netname  ../relu_image_models/ffn_relu/mnist-net_256x4.onnx --dataset mnist --domain deeppoly --epsilon 0.05 --complete True --timeout_milp 90 --subset comp  > results/FFN_relu/mnist_256x4_eps0.05.txt
 
-python3 -u . --netname  mnist-net_256x6.onnx --dataset mnist --domain deeppoly --epsilon 0.02  --timeout_milp 90 --subset comp --complete True > results/FFN_relu/mnist_256x6_eps0.02.txt
+python3 -u . --netname  ../relu_image_models/ffn_relu/mnist-net_256x6.onnx --dataset mnist --domain deeppoly --epsilon 0.02  --timeout_milp 90 --subset comp --complete True > results/FFN_relu/mnist_256x6_eps0.02.txt
 
-python3 -u . --netname  mnist-net_256x6.onnx --dataset mnist --domain deeppoly --epsilon 0.05 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x6_eps0.05.txt
+python3 -u . --netname  ../relu_image_models/ffn_relu/mnist-net_256x6.onnx --dataset mnist --domain deeppoly --epsilon 0.05 --complete True --timeout_milp 90 --subset comp > results/FFN_relu/mnist_256x6_eps0.05.txt
 
