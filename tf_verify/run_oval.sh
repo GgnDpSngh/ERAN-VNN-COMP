ac86f4df248613f716edb5b5eb267da349c5fc89
GgnDpSngh <shanky.22.singh@gmail.com>
2020-08-27 18:11:38 +0200

    imporved script

--- tf_verify/run_oval.sh
+++ tf_verify/run_oval.sh
@@ -1,4 +1,4 @@
-python3 -u . --domain refinepoly --netname relu_image_models/oval/cifar_deep_kw.onnx --dataset cifar10 --mean 0.485 0.456 0.406 --std 0.225 0.225 0.225 --timeout_lp 3600  --sparse_n 10 --subset base100 --target ../data/base_prop.csv --epsfile ../data/base_eps.csv --normalized_region False > results/oval/deep.txt
+python3 -u . --domain refinepoly --netname relu_image_models/oval/cifar_deep_kw.onnx --dataset cifar10 --mean 0.485 0.456 0.406 --std 0.225 0.225 0.225 --timeout_milp 3590  --sparse_n 10 --subset base100 --target ../data/base_prop.csv --epsfile ../data/base_eps.csv --normalized_region False > results/oval/deep.txt
 
 python3 -u . --domain deeppoly --netname relu_image_models/oval/cifar_wide_kw.onnx --dataset cifar10 --mean 0.485 0.456 0.406 --std 0.225 0.225 0.225 --timeout_milp 3590 --complete True --subset wide100 --target ../data/wide_prop.csv --epsfile ../data/wide_eps.csv --normalized_region False > results/oval/wide.txt
 
